58502ee8fd6b96ec4f84be9a1df6eeac84f91ca0
DevSrSouza <devsrsouza@gmail.com>
2019-09-20 14:48:30 -0300

    submiting head list with binding adapters

--- android/src/main/java/br/com/devsrsouza/mcheads/ui/home/BindingAdapters.kt
+++ android/src/main/java/br/com/devsrsouza/mcheads/ui/home/BindingAdapters.kt
@@ -2,13 +2,22 @@ package br.com.devsrsouza.mcheads.ui.home
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import br.com.devsrsouza.mcheads.R
 import br.com.devsrsouza.mcheads.common.Head
 import coil.api.load
 
-@BindingAdapter
-fun ImageView.headRender(head: Head) {
-    load(head.imageUrl) {
-        placeholder(R.drawable.default_head)
+@BindingAdapter("head_render")
+fun ImageView.headRender(head: Head?) {
+    head?.let {
+        load(it.imageUrl) {
+            placeholder(R.drawable.default_head)
+        }
     }
+}
+
+@BindingAdapter("head_grid_adapter")
+fun RecyclerView.recyclerViewGridAdapter(data: List<Head>) {
+    val adapter = adapter as HeadGridAdapter
+    adapter.submitList(data)
 }
--- android/src/main/java/br/com/devsrsouza/mcheads/ui/home/HomeFragment.kt
+++ android/src/main/java/br/com/devsrsouza/mcheads/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.get
 import br.com.devsrsouza.mcheads.databinding.FragmentHomeBinding
@@ -23,15 +22,9 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding = FragmentHomeBinding.inflate(inflater)
         binding.lifecycleOwner = this
-
         binding.viewModel = viewModel
-
-        val adapter = HeadGridAdapter()
-        binding.headList.adapter = adapter
-
-        viewModel.heads.observe(viewLifecycleOwner, Observer {
-            it?.let { adapter.submitList(it) }
-        })
+        binding.headList.adapter = HeadGridAdapter()
+        binding.executePendingBindings()
 
         return binding.root
     }
